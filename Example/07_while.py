315bccf6658a4b7d87c15e0fd6f5228e6f60fc1b
idektep <idektep.coach@gmail.com>
2025-12-05 21:16:25 +0700

    Update 07_while.py

--- Example/07_while.py
+++ Example/07_while.py
@@ -1,28 +1,4 @@
-# color=0
-# while 1:
-#     color=input("Enter color : ")
-#     if color=="red":
-#         print("stop")
-#     elif color=="yellow":
-#         print("slow down")
-#     elif color=="green":
-#         print("go")
-#     elif color=="exit":
-#         print("exit")
-#         break
-#     else:
-#         print("error")
-
-color=0
-while color!="exit":
-    color=input("Enter color : ")
-    if color=="red":
-        print("stop")
-    elif color=="yellow":
-        print("slow down")
-    elif color=="green":
-        print("go")
-    elif color=="exit":
-        print("exit")
-    else:
-        print("error")
+count=0
+while color!=10:
+    print(f"Round : {count}")
+    count=count+1
